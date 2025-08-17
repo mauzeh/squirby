488587c44432bd822d903576a9c45b3ea2b304a0
Maurits Dekkers <bluedackers@gmail.com>
2025-08-17 09:27:19 -0700

    feat: Order log entries from latest to earliest

--- app/Http/Controllers/DailyLogController.php
+++ app/Http/Controllers/DailyLogController.php
@@ -23,7 +23,7 @@ class DailyLogController extends Controller
 
         $dailyLogs = DailyLog::with(['ingredient', 'unit'])
             ->whereDate('created_at', $selectedDate->toDateString())
-            ->orderBy('created_at', 'asc')
+            ->orderBy('created_at', 'desc')
             ->get();
 
         $dailyTotals = $this->calculateDailyTotals($dailyLogs);
