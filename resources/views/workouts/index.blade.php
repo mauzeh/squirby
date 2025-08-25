2cc997475070cc136f4e67398e0172bba680a2b1
Maurits Dekkers <bluedackers@gmail.com>
2025-08-24 17:18:10 -0700

    fix: Handle newlines in comments for TSV export

--- resources/views/workouts/index.blade.php
+++ resources/views/workouts/index.blade.php
@@ -69,5 +69,24 @@
                 @endforeach
             </tbody>
         </table>
+
+        <div class="form-container">
+            <h3>TSV Export</h3>
+            <textarea id="tsv-output" rows="10" style="width: 100%; background-color: #3a3a3a; color: #f2f2f2; border: 1px solid #555;">Date	Time	Exercise	Weight (lbs)	Reps	Rounds	Comments
+@foreach ($workouts as $workout)
+{{ $workout->logged_at->format('Y-m-d') }}	{{ $workout->logged_at->format('H:i') }}	{{ $workout->exercise->title }}	{{ $workout->weight }}	{{ $workout->reps }}	{{ $workout->rounds }}	{{ preg_replace('/(\n|\r)+/', ' ', $workout->comments) }}
+@endforeach
+            </textarea>
+            <button id="copy-tsv-button" class="button">Copy to Clipboard</button>
+        </div>
+
+        <script>
+            document.getElementById('copy-tsv-button').addEventListener('click', function() {
+                var tsvOutput = document.getElementById('tsv-output');
+                tsvOutput.select();
+                document.execCommand('copy');
+                alert('TSV data copied to clipboard!');
+            });
+        </script>
     </div>
 @endsection
