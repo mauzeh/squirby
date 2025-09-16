46866d3fffc113b69b18c0b4c106023b6289f9ac
Maurits Dekkers <bluedackers@gmail.com>
2025-09-15 20:58:34 -0700

    feat: Apply responsive design to ingredients table

--- resources/views/ingredients/index.blade.php
+++ resources/views/ingredients/index.blade.php
@@ -23,21 +23,26 @@
                 <thead>
                     <tr>
                         <th>Name</th>
-                        <th>Quantity</th>
-                        <th>Calories</th>
-                        <th>P/C/F (g)</th>
-                        <th>Cost Per Unit</th>
+                        <th class="hide-on-mobile">Quantity</th>
+                        <th class="hide-on-mobile">Calories</th>
+                        <th class="hide-on-mobile">P/C/F (g)</th>
+                        <th class="hide-on-mobile">Cost Per Unit</th>
                         <th class="actions-column">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($ingredients as $ingredient)
                         <tr>
-                            <td>{{ $ingredient->name }}</td>
-                            <td>{{ $ingredient->base_quantity }}{{ $ingredient->baseUnit->abbreviation }}</td>
-                            <td>{{ round($ingredient->calories) }}</td>
-                            <td>{{ $ingredient->protein }}/{{ $ingredient->carbs }}/{{ $ingredient->fats }}</td>
-                            <td>{{ number_format($ingredient->cost_per_unit, 2) }}</td>
+                            <td>
+                                {{ $ingredient->name }}
+                                <div class="show-on-mobile" style="font-size: 0.9em; color: #ccc;">
+                                    {{ $ingredient->base_quantity }}{{ $ingredient->baseUnit->abbreviation }} - P:{{ $ingredient->protein }} C:{{ $ingredient->carbs }} F:{{ $ingredient->fats }}
+                                </div>
+                            </td>
+                            <td class="hide-on-mobile">{{ $ingredient->base_quantity }}{{ $ingredient->baseUnit->abbreviation }}</td>
+                            <td class="hide-on-mobile">{{ round($ingredient->calories) }}</td>
+                            <td class="hide-on-mobile">{{ $ingredient->protein }}/{{ $ingredient->carbs }}/{{ $ingredient->fats }}</td>
+                            <td class="hide-on-mobile">{{ number_format($ingredient->cost_per_unit, 2) }}</td>
                             <td class="actions-column">
                                 <div style="display: flex; gap: 5px;">
                                     <a href="{{ route('ingredients.edit', $ingredient->id) }}" class="button edit">Edit</a>
