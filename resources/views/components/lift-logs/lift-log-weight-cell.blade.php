920d2dfb4dd9d625bea632be89cd80d0472c9c69
Maurits Dekkers <bluedackers@gmail.com>
2025-10-15 12:33:54 -0700

    Make weight display larger and bold in lift-logs table
    - Increase weight font size to 1.3em in lift-log-weight-cell
    - Add bold font-weight to make weights more prominent
    - Improves readability of weight values in lift-logs index table

--- resources/views/components/lift-logs/lift-log-weight-cell.blade.php
+++ resources/views/components/lift-logs/lift-log-weight-cell.blade.php
@@ -1,6 +1,6 @@
 @props(['liftLog'])
 
 <td class="hide-on-mobile">
-    {{ $liftLog['formatted_weight'] }}<br>
+    <span style="font-size: 1.3em; font-weight: bold;">{{ $liftLog['formatted_weight'] }}</span><br>
     {{ $liftLog['formatted_reps_sets'] }}
 </td>
