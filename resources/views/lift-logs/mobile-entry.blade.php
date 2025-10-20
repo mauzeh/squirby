a3e15106c66f4927bf4c10a017da13ce1b855b9a
Maurits Dekkers <bluedackers@gmail.com>
2025-10-20 12:37:49 -0700

    Move Add exercise button to top of lift-logs mobile entry page
    - Repositioned Add exercise button above program cards for better UX
    - Moved exercise list container to top for easier access
    - Maintained all existing functionality and JavaScript interactions

--- resources/views/lift-logs/mobile-entry.blade.php
+++ resources/views/lift-logs/mobile-entry.blade.php
@@ -57,6 +57,46 @@
             </div>
         </div>
 
+        <div class="add-exercise-container">
+            <button type="button" id="add-exercise-button" class="button-large button-green">Add exercise</button>
+        </div>
+
+        <div class="item-list-container hidden" id="exercise-list-container">
+            <div class="item-list exercise-list">
+                <a href="#" id="new-exercise-link" class="item-list-item exercise-list-item new-exercise-item">✚ Create new exercise</a>
+                <div id="new-exercise-form-container" class="hidden">
+                    <form action="{{ route('programs.quick-create', ['date' => $selectedDate->toDateString()]) }}" method="POST" class="new-exercise-form">
+                        @csrf
+                        <input type="hidden" name="redirect_to" value="mobile-entry">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <input type="text" name="exercise_name" id="exercise_name" class="large-input large-input-text" required>
+                            </div>
+                        </div>
+                        <button type="submit" class="button-large button-green">Add Exercise</button>
+                    </form>
+                </div>
+                
+                @if (!empty($recommendations))
+                    @foreach ($recommendations as $recommendation)
+                        <a href="{{ route('programs.quick-add', ['exercise' => $recommendation['exercise']->id, 'date' => $selectedDate->toDateString(), 'redirect_to' => 'mobile-entry']) }}" class="item-list-item exercise-list-item recommended-exercise">
+                            <span class="item-name exercise-name">{{ $recommendation['exercise']->title }}</span>
+                            <span class="item-label exercise-label">⭐ <em>Recommended</em></span>
+                        </a>
+                    @endforeach
+                @endif
+                
+                @foreach ($exercises as $exercise)
+                    <a href="{{ route('programs.quick-add', ['exercise' => $exercise->id, 'date' => $selectedDate->toDateString(), 'redirect_to' => 'mobile-entry']) }}" class="item-list-item exercise-list-item {{ $exercise->is_user_created ? 'user-exercise' : '' }}">
+                        <span class="item-name exercise-name">{{ $exercise->title }}</span>
+                        @if($exercise->is_user_created)
+                            <span class="item-label exercise-label"><em>Created by you</em></span>
+                        @endif
+                    </a>
+                @endforeach
+            </div>
+        </div>
+
         @if ($programs->isEmpty())
             <p class="no-program-message">No program entries for this day.</p>
         @else
@@ -204,46 +244,6 @@
                 </div>
             @endforeach
         @endif
-
-        <div class="add-exercise-container">
-            <button type="button" id="add-exercise-button" class="button-large button-green">Add exercise</button>
-        </div>
-
-        <div class="item-list-container hidden" id="exercise-list-container">
-            <div class="item-list exercise-list">
-                <a href="#" id="new-exercise-link" class="item-list-item exercise-list-item new-exercise-item">✚ Create new exercise</a>
-                <div id="new-exercise-form-container" class="hidden">
-                    <form action="{{ route('programs.quick-create', ['date' => $selectedDate->toDateString()]) }}" method="POST" class="new-exercise-form">
-                        @csrf
-                        <input type="hidden" name="redirect_to" value="mobile-entry">
-                        <div class="form-group">
-                            <div class="input-group">
-                                <input type="text" name="exercise_name" id="exercise_name" class="large-input large-input-text" placeholder="Enter new exercise name..." required>
-                            </div>
-                        </div>
-                        <button type="submit" class="button-large button-green">Add Exercise</button>
-                    </form>
-                </div>
-                
-                @if (!empty($recommendations))
-                    @foreach ($recommendations as $recommendation)
-                        <a href="{{ route('programs.quick-add', ['exercise' => $recommendation['exercise']->id, 'date' => $selectedDate->toDateString(), 'redirect_to' => 'mobile-entry']) }}" class="item-list-item exercise-list-item recommended-exercise">
-                            <span class="item-name exercise-name">{{ $recommendation['exercise']->title }}</span>
-                            <span class="item-label exercise-label">⭐ <em>Recommended</em></span>
-                        </a>
-                    @endforeach
-                @endif
-                
-                @foreach ($exercises as $exercise)
-                    <a href="{{ route('programs.quick-add', ['exercise' => $exercise->id, 'date' => $selectedDate->toDateString(), 'redirect_to' => 'mobile-entry']) }}" class="item-list-item exercise-list-item {{ $exercise->is_user_created ? 'user-exercise' : '' }}">
-                        <span class="item-name exercise-name">{{ $exercise->title }}</span>
-                        @if($exercise->is_user_created)
-                            <span class="item-label exercise-label"><em>Created by you</em></span>
-                        @endif
-                    </a>
-                @endforeach
-            </div>
-        </div>
     </div>
 
 
