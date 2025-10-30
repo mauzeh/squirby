55f113490e577dd668c00e42daad5506a4b20414
Maurits Dekkers <bluedackers@gmail.com>
2025-10-29 23:44:29 -0700

    Add graph button to lift logs actions
    - Add chart-line icon button to view exercise logs/analytics
    - Button links to exercise/{id}/logs page for detailed charts
    - Positioned before edit/delete buttons with blue styling
    - Includes tooltip for accessibility

--- resources/views/components/lift-logs/actions-cell.blade.php
+++ resources/views/components/lift-logs/actions-cell.blade.php
@@ -2,6 +2,7 @@
 
 <td class="actions-column">
     <div class="actions-flex">
+        <a href="{{ $liftLog['exercise_url'] }}" class="button" style="background-color: #007bff;" title="View Exercise Logs"><i class="fa-solid fa-chart-line"></i></a>
         <a href="{{ $liftLog['edit_url'] }}" class="button edit"><i class="fa-solid fa-pencil"></i></a>
         <form action="{{ route('lift-logs.destroy', $liftLog['id']) }}" method="POST" style="display:inline;">
             @csrf
